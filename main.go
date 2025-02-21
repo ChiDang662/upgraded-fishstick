5648b34b6b75199da2185788baea9b33a64c8342
Dang Thao Chi <96994027+ChiDang662@users.noreply.github.com>
2025-02-21 17:14:28 +0000

    Add initial project structure with Cobra and Viper integration

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "app",
+	Short: "A brief description of your application",
+	Long:  `A longer description of your application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, Cobra and Viper!")
+	},
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.app.yaml)")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".app")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func main() {
+	cobra.CheckErr(rootCmd.Execute())
+}
